442bf8bd041015ff100add195d2b82b587ca1016
M-Harley01 <190006115@dundee.ac.uk>
2025-01-22 14:10:34 +0000

    forwarding to switch started

--- atmUtil.h
+++ atmUtil.h
@@ -8,6 +8,11 @@
 #include <vector>
 #include "raygui.h"
 
+#define TRANSACTION_SUCESS 0
+#define INSUFFICIENT_FUNDS 1
+#define DATABASE_ERROR 2
+#define INCORRECT_PIN 3
+
 using namespace std;
 
 struct Account {
@@ -19,6 +24,21 @@ typedef struct {
     Rectangle bounds; 
     const char *text;
 } Button;
+
+struct Transaction{
+    char cardNumber[20];
+    char expiryDate[6];
+    uint64_t atmID;
+    uint64_t uniqueTransactionID;
+    char pinNo[5];
+    double withdrawalAmount;
+};
+
+struct Response {
+    int succeeded;
+    double new_balance;
+};
+
 
 
 bool validatePIN(string enteredPIN);
@@ -42,4 +62,6 @@ void printFunction(string balance);
 
 void processingScreen(string messageToPrint);
 
+void forwardToSocket(string cardNumber, string expiryDate, string transactionID, string atmID, string pin, double withdrawalAmount);
+ 
 #endif
