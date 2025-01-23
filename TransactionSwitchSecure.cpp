493fabac04229bec3bad2ed02643c38fcd4980b1
Drew Webster <d.webster4034@gmail.com>
2025-01-23 15:18:51 +0000

    Update TransactionSwitchSecure.cpp

--- TransactionSwitchSecure.cpp
+++ TransactionSwitchSecure.cpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <vector>
+#include <thread>
+#include <mutex>
 #include "TransactionLogr.h"
 #include "Transaction.h"
 #include "NetworkRouter.h"
@@ -51,48 +53,6 @@ public:
         return sock;
     }
 
-    struct SSLConnection {
-        SSL *ssl;
-        int sock;
-    };
-
-    SSLConnection connectToNetwork(int port) {
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock < 0) {
-            std::cerr << "Failed to create socket\n";
-            return {nullptr, -1};
-        }
-
-        sockaddr_in serv_addr;
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(port);
-
-        if (inet_pton(AF_INET, "10.201.102.155", &serv_addr.sin_addr) <= 0) {
-            std::cerr << "Invalid address\n";
-            close(sock);
-            return {nullptr, -1};
-        }
-
-        if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-            std::cerr << "Failed to connect to Network\n";
-            close(sock);
-            return {nullptr, -1};
-        }
-
-        SSL *ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, sock);
-
-        if (SSL_connect(ssl) != 1) {
-            std::cerr << "Failed to establish SSL connection to Network\n";
-            ERR_print_errors_fp(stderr);
-            SSL_free(ssl);
-            close(sock);
-            return {nullptr, -1};
-        }
-
-        return {ssl, sock};
-    }
-
     void startServer() {
         int server_sock_6667 = createSocket(6667);
 
@@ -105,18 +65,8 @@ public:
                 continue;
             }
 
-            SSL *ssl = SSL_new(ctx);
-            SSL_set_fd(ssl, atm_socket);
-
-            if (SSL_accept(ssl) <= 0) {
-                ERR_print_errors_fp(stderr);
-            } else {
-                handleAtmRequest(ssl);
-            }
-
-            SSL_shutdown(ssl);
-            SSL_free(ssl);
-            close(atm_socket);
+            // Create a thread to handle the client connection
+            std::thread(&TransactionSwitch::handleConnection, this, atm_socket).detach();
         }
 
         close(server_sock_6667);
@@ -126,6 +76,8 @@ private:
     TransactionLogger logger;
     NetworkRouter router;
     SSL_CTX *ctx;
+    std::mutex logger_mutex; // Mutex for thread-safe access to logger
+    std::mutex router_mutex; // Mutex for thread-safe access to router
 
     SSL_CTX *createSSLContext() {
         const SSL_METHOD *method = TLS_server_method();
@@ -147,6 +99,21 @@ private:
         return ctx;
     }
 
+    void handleConnection(int atm_socket) {
+        SSL *ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, atm_socket);
+
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        } else {
+            handleAtmRequest(ssl);
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(atm_socket);
+    }
+
     void handleAtmRequest(SSL *ssl) {
         char buffer[sizeof(Transaction)];
 
@@ -159,126 +126,88 @@ private:
             Transaction message;
             memcpy(&message, buffer, sizeof(Transaction));
 
-            std::cout << "Received: " << message.cardNumber << std::endl;
+            {
+                std::lock_guard<std::mutex> lock(logger_mutex);
+                std::cout << "Received: " << message.cardNumber << std::endl;
+            }
 
-            Response response = sendNetworkMessage(message);       
-
-
-            
-                SSL_write(ssl, &response, sizeof(Response));
-            
-
+            Response response = sendNetworkMessage(message);
+            SSL_write(ssl, &response, sizeof(Response));
         }
     }
 
-    Response sendNetworkMessage(Transaction message){
+    Response sendNetworkMessage(Transaction message) {
         const int DATABASE_ERROR = 1;
-const char *host = "10.201.102.155"; // Server address
-    const int port = 6668;         // Server port
+        const char *host = "10.201.102.155"; // Server address
+        const int port = 6668;              // Server port
 
-    // Initialise OpenSSL
-    SSL_library_init();
-    SSL_load_error_strings();
-    const SSL_METHOD *method = TLS_client_method();
-    SSL_CTX *ctx = SSL_CTX_new(method);
+        SSL_CTX *client_ctx = SSL_CTX_new(TLS_client_method());
+        if (!client_ctx) {
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
 
-    if (!ctx) {
-        //cerr << "Failed to create SSL context" << endl;
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            SSL_CTX_free(client_ctx);
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
+
+        sockaddr_in server_addr = {};
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+        if (inet_pton(AF_INET, host, &server_addr.sin_addr) <= 0) {
+            close(sock);
+            SSL_CTX_free(client_ctx);
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
+
+        if (connect(sock, (sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            close(sock);
+            SSL_CTX_free(client_ctx);
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
+
+        SSL *ssl = SSL_new(client_ctx);
+        SSL_set_fd(ssl, sock);
+
+        if (SSL_connect(ssl) <= 0) {
+            SSL_free(ssl);
+            close(sock);
+            SSL_CTX_free(client_ctx);
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
+
+        if (SSL_write(ssl, &message, sizeof(message)) <= 0) {
+            SSL_free(ssl);
+            close(sock);
+            SSL_CTX_free(client_ctx);
+            Response response;
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
+
         Response response;
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
+        if (SSL_read(ssl, &response, sizeof(response)) <= 0) {
+            SSL_free(ssl);
+            close(sock);
+            SSL_CTX_free(client_ctx);
+            response.succeeded = DATABASE_ERROR;
+            return response;
+        }
 
-    // Create a socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        //cerr << "Failed to create socket" << endl;
-        SSL_CTX_free(ctx);
-        Response response;
-
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
-
-    // Set up the server address struct
-    sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    if (inet_pton(AF_INET, host, &server_addr.sin_addr) <= 0) {
-        //cerr << "Invalid server address" << endl;
-        close(sock);
-        SSL_CTX_free(ctx);
-        Response response;
-
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
-
-    // Connect to the server
-    if (connect(sock, (sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        //cerr << "Failed to connect to the server" << endl;
-        close(sock);
-        SSL_CTX_free(ctx);
-        Response response;
-
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
-
-    // Wrap the socket with SSL
-    SSL *ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, sock);
-
-    if (SSL_connect(ssl) <= 0) {
-        //cerr << "TLS handshake failed" << endl;
         SSL_free(ssl);
         close(sock);
-        SSL_CTX_free(ctx);
-        Response response;
-
-        response.succeeded = DATABASE_ERROR;
+        SSL_CTX_free(client_ctx);
         return response;
-    }
-
-    //cout << "Connected to the server via TLS" << endl;
-
-    // Prepare the transaction struct
-
-    // Send the transaction struct to the server
-    if (SSL_write(ssl, &message, sizeof(message)) <= 0) {
-        //cerr << "Failed to send transaction to the server" << endl;
-        SSL_free(ssl);
-        close(sock);
-        SSL_CTX_free(ctx);
-        Response response;
-
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
-
-    // Receive the response struct from the server
-    Response response;
-    if (SSL_read(ssl, &response, sizeof(response)) <= 0) {
-        //cerr << "Failed to receive response from the server" << endl;
-        SSL_free(ssl);
-        close(sock);
-        SSL_CTX_free(ctx);
-        response.succeeded = DATABASE_ERROR;
-        return response;
-    }
-
-    // Print the response
-    //cout << "Transaction Response:" << endl;
-    //cout << "  Succeeded: " << response.succeeded << endl;
-    //cout << "  New Balance: " << response.new_balance << endl;
-
-    // Clean up
-    SSL_free(ssl);
-    close(sock);
-    SSL_CTX_free(ctx);
-    std::cout << "Response sent"<<std::endl;
-    return response;
-
     }
 };
