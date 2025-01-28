45cbe2384ee9a5bdb495f93fa154c3ea616a6d3b
Cieran0 <cieran999@gmail.com>
2025-01-28 20:05:37 +0000

    Forgot to add Currency.hpp

--- network simulator/Currency.hpp
+++ network simulator/Currency.hpp
@@ -25,5 +25,10 @@ extern std::unordered_map<Currency, DecimalPosition> currencyDotPosition;
 extern const std::unordered_map<std::string, Currency> currencyCodeToEnum;
 extern const std::unordered_map<Currency, std::string> enumToCurrencyCode;
 
+enum Rounding {
+    UP,
+    DOWN
+};
+
 int initCurrencyPositions(sqlite3*& db);
-int ConvertCurrency(Currency from, Currency to, int64_t amountBefore, int64_t& amountAfter);
+int ConvertCurrency(const Currency& from, const Currency& to, const int64_t& amountBefore, int64_t& amountAfter, const Rounding& rounding);
