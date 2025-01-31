5ce75d385c0dd2d9643e2c9fdc0b18dc333d8160
Cieran0 <cieran999@gmail.com>
2025-01-31 12:31:15 +0000

    On other thread

--- transactionSwitch/TransactionLogr.cpp
+++ transactionSwitch/TransactionLogr.cpp
@@ -5,82 +5,51 @@
 #include <chrono>
 #include <unordered_map>
 #include <mutex>
+#include <thread>
+#include <queue>
+#include <condition_variable>
 
 std::mutex logMutex;
+std::queue<Transaction> transactionQueue;
+std::condition_variable logCondition;
+bool stopLogging = false;
+std::thread loggingThreadInstance;
 
 
-const std::unordered_map<Currency, std::string> enumToCurrencyCode = {
-    {GBP, "GBP"},
-    {USD, "USD"},
-    {JPY, "JPY"},
-    {EUR, "EUR"},
-    {AUD, "AUD"},
-    {CAD, "CAD"},
-    {CHF, "CHF"},
-    {CNH, "CNH"},
-    {HKD, "HKD"},
-    {NZD, "NZD"}
-};
-
-std::string transactionToString(const Transaction& transaction) {
-    std::string message;
-    if(transaction.type == BALANCE_CHECK){ 
-        message = std::format("[{}]: Transaction ID {} | Card number [{}] checked balance at ATM {} {}",
-            std::chrono::system_clock::now(),
-            transaction.id, 
-            transaction.amount,
-            transaction.cardNumber,
-            transaction.atmID
-        );
+void loggingThread(TransactionLogger* logger) {
+    while (true) {
+        std::unique_lock<std::mutex> lock(logMutex);
+        logCondition.wait(lock, [] { return !transactionQueue.empty() || stopLogging; });
+        
+        while (!transactionQueue.empty()) {
+            Transaction transaction = transactionQueue.front();
+            transactionQueue.pop();
+            lock.unlock();
+            logger->logTransaction(transaction);
+            lock.lock();
+        }
+        
+        if (stopLogging) break;
     }
-    else if(transaction.type == DEPOSIT){   
-        message = std::format("[{}]: Transaction ID {} | Deposit of {} {} with card [{}] at ATM {}",
-            std::chrono::system_clock::now(),
-            transaction.id, 
-            transaction.amount,
-            enumToCurrencyCode.at(transaction.currency),
-            transaction.cardNumber,
-            transaction.atmID
-        );
-    } else if (transaction.type == WITHDRAWL) {
-        message = std::format("[{}]: Transaction ID {} | withdrawal of {} {} with card [{}] at ATM {}",
-            std::chrono::system_clock::now(),
-            transaction.id, 
-            transaction.amount,
-            enumToCurrencyCode.at(transaction.currency),
-            transaction.cardNumber,
-            transaction.atmID
-        );
-
-    } else if (transaction.type == TranscationType::PIN_CHECK) {
-        message = std::format("[{}]: Transaction ID {} | pin check with card [{}] at ATM {}",
-            std::chrono::system_clock::now(),
-            transaction.id, 
-            transaction.cardNumber,
-            transaction.atmID
-        );
-    }
-    else{
-        message = std::format("[{}]: Transaction ID {} | transaction of {} {} with card [{}] at ATM {}",
-            std::chrono::system_clock::now(),
-            transaction.id, 
-            transaction.amount,
-            enumToCurrencyCode.at(transaction.currency),
-            transaction.cardNumber,
-            transaction.atmID
-        );
-    }
-    return message;
-} 
+}
 
 TransactionLogger::TransactionLogger() {
     logFile.open("transaction_log.txt", std::ios::app);
     if (!logFile.is_open()) {
         std::cerr << "Error opening log file!" << std::endl;
     }
+    loggingThreadInstance = std::thread(loggingThread, this);
 }
 
 TransactionLogger::~TransactionLogger() {
+    {
+        std::lock_guard<std::mutex> lock(logMutex);
+        stopLogging = true;
+    }
+    logCondition.notify_one();
+    if (loggingThreadInstance.joinable()) {
+        loggingThreadInstance.join();
+    }
     if (logFile.is_open()) {
         logFile.close();
     }
@@ -88,13 +57,6 @@ TransactionLogger::~TransactionLogger() {
 
 void TransactionLogger::logTransaction(const Transaction& transaction) {
     std::lock_guard<std::mutex> lock(logMutex);
-
-    std::string transactionDetails = transactionToString(transaction);
-
-    if (logFile.is_open()) {
-        logFile << transactionDetails << std::endl;
-        std::cout << transactionDetails << std::endl;
-    } else {
-        std::cerr << "Failed to log transaction: " << transactionDetails << std::endl;
-    }
+    transactionQueue.push(transaction);
+    logCondition.notify_one();
 }
